1b0845e554f761f07c9233fb2956f20c33f6d05b
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2020-11-23 10:13:58 +0800

    Update configs

--- app/interactors/validate_sign_in_params.rb
+++ app/interactors/validate_sign_in_params.rb
@@ -8,7 +8,7 @@ class ValidateSignInParams
     @user = User.find_by(email: credentials[:email])
 
     # ensures we have the correct user
-    context.fail!(error: @user.errors.full_messages.to_sentence) unless @user
+    context.fail!(error: "Record not found") unless @user
     context.fail!(error: "Invalid credentials.") unless @user.valid_password?(credentials[:password])
     context.user = @user
   end
